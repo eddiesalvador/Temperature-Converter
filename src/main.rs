15a03bcebfd1e8396ec00cda1045d59e2b842fc3
Discovery <71903910+eddiesalvador@users.noreply.github.com>
2022-07-14 03:30:00 -0400

    Added a loop
    This allows the user to reuse the program until they themselves end the program by entering a specific input.

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,69 @@
 use std::io;
 
 fn main() {
-    // Ask user what type of tempature conversion they want
-    println!("Hello, this program will convert temperatures!");
-    println!("Type \"1\" for Celsius to Fahrenheit.");
-    println!("Type \"2\" for Celsius to Kelvin.");
-    println!("Type \"3\" for Fahrenheit to Celsius.");
-    println!("Type \"4\" for Fahrenheit to Kelvin.");
-    println!("Type \"5\" for Kelvin to Fahrenheit.");
-    println!("Type \"6\" for Kelvin to Celsius.");
+    // loop until user exits infinite loop via a break
+    loop {
+        // Ask user what type of tempature conversion they want
+        println!("Hello, this program will convert temperatures!");
+        println!("Type \"1\" for Celsius to Fahrenheit.");
+        println!("Type \"2\" for Celsius to Kelvin.");
+        println!("Type \"3\" for Fahrenheit to Celsius.");
+        println!("Type \"4\" for Fahrenheit to Kelvin.");
+        println!("Type \"5\" for Kelvin to Fahrenheit.");
+        println!("Type \"6\" for Kelvin to Celsius.");
 
-    // Create a user input 
-    let mut input_unit = String::new();
-    io::stdin()
-        .read_line(&mut input_unit)
-        .expect("Failed to read line");
+        // Inform user they can exit the program
+        println!("Type \"0\" to exit the program.");
 
-    // Convert the input to u32
-    let int_input_unit: u32 = input_unit.trim().parse().expect("f32 failed");
+        // Create a user input 
+        let mut input_unit = String::new();
+        io::stdin()
+            .read_line(&mut input_unit)
+            .expect("Failed to read line");
 
-    // Ask user the value of the unit
-    println!("What is the value of the unit?");
-    
-    // Create a user intput
-    let mut input_value = String::new();
-    io::stdin()
-        .read_line(&mut input_value)
-        .expect("Failed to read line");
-    
-    // Convert the input to f32
-    let int_input_value: f32 = input_value.trim().parse().expect("f32 failed");
+        // Convert the input to u32
+        let int_input_unit: u32 = input_unit.trim().parse().expect("f32 failed");
 
-    // Conditionals, after a valid number is selected
-    // the values are computed and printed
-    if int_input_unit == 1 {
-        println!("{} degrees fahrenheit", celsius_to_fahrenheit(int_input_value));
-    } else if int_input_unit == 2 {
-        println!("{} degrees kelvin", celsius_to_kelvin(int_input_value));
-    } else if int_input_unit == 3 {
-        println!("{} degrees celsius", fahrenheit_to_celsius(int_input_value));
-    } else if int_input_unit == 4 {
-        println!("{} degrees kelivn", fahrenheit_to_kelvin(int_input_value));
-    } else if int_input_unit == 5 {
-        println!("{} degrees fahrenheit", kelvin_to_fahrenheit(int_input_value));
-    } else if int_input_unit == 6 {
-        println!("{} degrees celsius", kelvin_to_celsius(int_input_value));
-    } else {
-        println!("This is not a valid option");
+        // Check if user decided to exit program before 
+        // asking the user for a second input
+        if int_input_unit == 0 {
+            break;
+        }
+
+        // Ask user the value of the unit
+        println!("What is the value of the unit?");
+        println!("For example, \"81.5\" & \"32\" are acceptable inputs.");
+        
+
+        // Create a user intput
+        let mut input_value = String::new();
+        io::stdin()
+            .read_line(&mut input_value)
+            .expect("Failed to read line");
+        
+        // Convert the input to f32
+        let int_input_value: f32 = input_value.trim().parse().expect("f32 failed");
+
+        // Conditionals, after a valid number is selected
+        // the values are computed and printed
+        if int_input_unit == 1 {
+            println!("{} degrees fahrenheit", celsius_to_fahrenheit(int_input_value));
+        } else if int_input_unit == 2 {
+            println!("{} degrees kelvin", celsius_to_kelvin(int_input_value));
+        } else if int_input_unit == 3 {
+            println!("{} degrees celsius", fahrenheit_to_celsius(int_input_value));
+        } else if int_input_unit == 4 {
+            println!("{} degrees kelivn", fahrenheit_to_kelvin(int_input_value));
+        } else if int_input_unit == 5 {
+            println!("{} degrees fahrenheit", kelvin_to_fahrenheit(int_input_value));
+        } else if int_input_unit == 6 {
+            println!("{} degrees celsius", kelvin_to_celsius(int_input_value));
+        } else {
+            println!("This is not a valid option");
+        }
     }
-    
+    println!("Thank you for using my temperature converter");
+            
 }
 
 // Below are the equations used to convert the tempature values
